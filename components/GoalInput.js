5c4b1d5f05bad365148ccac05fab9d0136381251
Andrea Fourel <andreafourel@hotmail.fr>
2023-10-10 11:33:32 +0200

    add Modal and handle opening/closing modal

--- components/GoalInput.js
+++ components/GoalInput.js
@@ -1,9 +1,9 @@
-import { StyleSheet, View, Button, TextInput } from "react-native";
+import { StyleSheet, View, Button, TextInput, Modal } from "react-native";
 import { useState } from 'react';
 
 function GoalInput(props){
 
-  const [enteredGoalText, setEnteredGoalText] = useState('');
+   const [enteredGoalText, setEnteredGoalText] = useState('');
   
   function goalInputHandler(enteredText){
     setEnteredGoalText(enteredText);
@@ -15,18 +15,30 @@ function GoalInput(props){
   }
 
   return(
-    <View style={styles.inputContainer}>
+    <Modal visible={props.visible} animationType="slide">
+      <View style={styles.inputContainer}>
         <TextInput 
           style={styles.textInput} 
           placeholder='Your cours goal'
           onChangeText={goalInputHandler}
           value={enteredGoalText}
         />
-        <Button 
-          title='Add goal'
-          onPress={addGoalHandler}
-        />
+        <View style={styles.buttonContainer}>
+          <View style={styles.button}>
+            <Button 
+             title='Add goal'
+              onPress={addGoalHandler}
+            />
+          </View>  
+          <View style={styles.button}>
+            <Button 
+              title="Cancel"
+              onPress={props.onCancel}
+            />  
+          </View>
+        </View>
       </View>
+    </Modal>
   );
 }
 
@@ -35,18 +47,25 @@ export default GoalInput;
 const styles = StyleSheet.create({
   inputContainer: {
     flex: 1,
-    justifyContent: 'space-between',
-    flexDirection: 'row',
+    justifyContent: 'center',
     alignItems: 'center',
     marginBottom: 24,
     borderBottomWidth: 1,
-    borderBottomColor: '#cccccc'
+    borderBottomColor: '#cccccc',
+    padding: 16
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    marginTop: 16
   },
   textInput: {
     borderWidth: 1,
     borderColor: '#cccccc',
-    width: '70%',
-    marginRight: 8,
+    width: '100%',
     padding: 8
   },
+  button:{
+    width: 100,
+    marginHorizontal: 8
+  }
 })
