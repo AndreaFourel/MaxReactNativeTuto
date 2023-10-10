42adc52e5b332290c940cf411c32c1e458467ba2
Andrea Fourel <andreafourel@hotmail.fr>
2023-10-10 11:55:42 +0200

    add image component and more styles to the app

--- components/GoalInput.js
+++ components/GoalInput.js
@@ -1,4 +1,4 @@
-import { StyleSheet, View, Button, TextInput, Modal } from "react-native";
+import { StyleSheet, View, Button, TextInput, Modal, Image } from "react-native";
 import { useState } from 'react';
 
 function GoalInput(props){
@@ -17,6 +17,10 @@ function GoalInput(props){
   return(
     <Modal visible={props.visible} animationType="slide">
       <View style={styles.inputContainer}>
+        <Image 
+          source={require('../assets/images/goal.png')}
+          style={styles.image}
+        />
         <TextInput 
           style={styles.textInput} 
           placeholder='Your cours goal'
@@ -26,16 +30,18 @@ function GoalInput(props){
         <View style={styles.buttonContainer}>
           <View style={styles.button}>
             <Button 
-             title='Add goal'
-              onPress={addGoalHandler}
-            />
-          </View>  
-          <View style={styles.button}>
-            <Button 
               title="Cancel"
               onPress={props.onCancel}
+              color='#f31282'
             />  
           </View>
+          <View style={styles.button}>
+            <Button 
+              title='Add goal'
+              onPress={addGoalHandler}
+              color='#b180f0'
+            />
+          </View>  
         </View>
       </View>
     </Modal>
@@ -49,10 +55,8 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    marginBottom: 24,
-    borderBottomWidth: 1,
-    borderBottomColor: '#cccccc',
-    padding: 16
+    padding: 16,
+    backgroundColor: '#311b6b'
   },
   buttonContainer: {
     flexDirection: 'row',
@@ -60,12 +64,20 @@ const styles = StyleSheet.create({
   },
   textInput: {
     borderWidth: 1,
-    borderColor: '#cccccc',
+    borderColor: '#e4d0ff',
+    backgroundColor : '#e4d0ff',
+    color:'#120438',
     width: '100%',
-    padding: 8
+    borderRadius: 6,
+    padding: 16
   },
   button:{
     width: 100,
     marginHorizontal: 8
+  }, 
+  image: {
+    width: 100,
+    height: 100,
+    margin: 20,
   }
 })
